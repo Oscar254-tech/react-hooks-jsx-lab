1722e8416415245e423e43fd91a84e556b9e7b1b
oscar ochanda <oscarochanda@gmail.com>
2025-08-04 21:57:43 +0300

    react-hooks-jsx-lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data"; // Changed from '../data/user'
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I love coding and building web applications!</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
